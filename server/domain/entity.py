42b71a2b15ae05a93dd26aa7ae0a146b8a467cab
Insiro <leeon@insiro.me>
2024-09-12 00:27:27 +0900

    .

--- server/domain/entity.py
+++ server/domain/entity.py
@@ -14,13 +14,15 @@ class Scrap(Base):
     __tablename__ = "scraps"
 
     id = Column(Integer, primary_key=True, index=True)
-    url = Column(String, unique=True, index=True)
-    source = Column(String)
+    url = Column(String(255), unique=True, index=True)
+    source = Column(String(50))
     content = Column(Text, nullable=True)
-    author_name = Column(String)
-    author_tag = Column(String)
-    images: Mapped[List["Image"]] = relationship("Image", back_populates="scrap", uselist=True)
-    comment = Column(String, nullable=True)
+    author_name = Column(String(100))
+    author_tag = Column(String(100))
+    images: Mapped[List["Image"]] = relationship(
+        "Image", back_populates="scrap", uselist=True
+    )
+    comment = Column(String(255), nullable=True)
 
     def __repr__(self):
         return f"<Scrap(id={self.id}, url={self.url}, author_info={self.author_name}@{self.author_tag})>"
@@ -30,7 +32,7 @@ class Image(Base):
     __tablename__ = "images"
 
     id = Column(Integer, primary_key=True, index=True)
-    file_name = Column(String)
+    file_name = Column(String(255))
     scrap_id = Column(Integer, ForeignKey("scraps.id"))
 
     scrap: Mapped[Scrap] = relationship("Scrap", back_populates="images")
